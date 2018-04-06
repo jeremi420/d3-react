cdc43e83d79198829c3d2b4a8392dc98fb5988db
Jeremi Wielewski <jerwier420@gmail.com>
2018-04-06 11:52:53 +0200

    dodano oś x oraz padding do wykresu

--- src/BarChart.js
+++ src/BarChart.js
@@ -4,15 +4,23 @@ import PropTypes from "prop-types";
 import { select } from "d3-selection";
 import { max } from "d3-array";
 import { scaleLinear } from "d3-scale";
+import { axisBottom } from "d3-axis";
 
 const SVG_REF = "svg";
-// const BAR_COLOR = "#fe9922";
 const BAR_WIDTH = 25;
+const DEFAULT_PADDING = 50;
 
 class BarChart extends Component {
 	render() {
 		const { width, height } = this.props;
-		return <svg width={width} height={height} ref={SVG_REF} />;
+		const padding = this.props.padding || DEFAULT_PADDING;
+		return (
+			<svg
+				width={width + padding * 2}
+				height={height + padding * 2}
+				ref={SVG_REF}
+			/>
+		);
 	}
 
 	componentDidMount() {
@@ -24,11 +32,20 @@ class BarChart extends Component {
 	}
 
 	renderChart() {
-		const { data, width } = this.props;
+		const { data, width, height } = this.props;
+		const padding = this.props.padding || DEFAULT_PADDING;
 		const maxVal = max(data);
 		const yScale = scaleLinear()
 			.domain([0, maxVal])
 			.range([0, width]);
+		const xScale = scaleLinear()
+			.domain([0, 20])
+			.range([0, width]);
+		select(SVG_REF)
+			.append("g")
+			.attr("postion", "fixed")
+			.attr("transform", `translate(${padding}, ${height + padding})`)
+			.call(axisBottom(xScale).ticks(20));
 		select(SVG_REF)
 			.selectAll("rect")
 			.data(data)
@@ -43,8 +60,8 @@ class BarChart extends Component {
 			.selectAll("rect")
 			.data(data)
 			.attr("class", "chartBar")
-			.attr("x", (d, i) => i * BAR_WIDTH)
-			.attr("y", d => width - yScale(d))
+			.attr("x", (d, i) => i * BAR_WIDTH + padding)
+			.attr("y", d => height + padding - yScale(d))
 			.attr("height", d => yScale(d))
 			.attr("width", BAR_WIDTH);
 	}
